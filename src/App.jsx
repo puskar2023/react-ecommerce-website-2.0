9e929311d9843888560cbb1909ac64b15d3161a5
puskar2023 <puskarmandal04@gmail.com>
2026-01-01 14:40:49 +0530

    adding routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,25 @@
 import React from "react";
-import Navbar from './components/Navbar'
-import ProductList from './pages/ProductList'
-import ProductDetail from './pages/ProductDetail'
+import Navbar from "./components/Navbar";
+import ProductList from "./pages/ProductList";
+import ProductDetail from "./pages/ProductDetail";
+import Cart from "./pages/Cart";
+import Checkout from "./pages/Checkout";
+import Footer from "./components/Footer";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 const App = () => {
   return (
     <>
       <BrowserRouter>
-      <div className="min-h-screen bg-gray-950 font-sans">
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<ProductList />} />
-          <Route path="/product/:id" element={<ProductDetail />} />
-        </Routes>
+        <div className="min-h-screen bg-gray-950 font-sans">
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<ProductList />} />
+            <Route path="/product/:id" element={<ProductDetail />} />
+            <Route path="/cart" element={<Cart />} />
+            <Route path="/checkout" element={<Checkout />} />
+          </Routes>
+          <Footer />
         </div>
       </BrowserRouter>
     </>
